4e2a344b178bf299f6a7fe7e00f78f2184b958be
ryanguyen2 <rnguyen2102@gmail.com>
2025-08-04 13:41:46 -0400

    fetched player stats from all games played

--- src/App.js
+++ src/App.js
@@ -23,17 +23,30 @@ class App extends Component {
       .catch(err => {
         console.log(err);
       });
+  }
 
+  getPlayerStats = () => {
+    axios.get("https://api.balldontlie.io/v1/stats?player_ids[]=237&seasons[]=2023&per_page=100", {
+      headers: {
+        Authorization: `Bearer ${apiKey}`
+      }
+    })
+      .then(async res => {
+        console.log(res.data.data)
+      }).catch(err => {
+        console.log(err)
+      })
   }
 
   componentDidMount() {
     this.getPlayerId()
+    this.getPlayerStats()
   }
 
 
   render() {
     return (
-      <div className="App">
+      <div className="App" >
 
       </div>
     );
