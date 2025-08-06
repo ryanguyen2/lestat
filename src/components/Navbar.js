8a705ad41d9655546751392fc2c7114525cac32c
ryanguyen2 <rnguyen2102@gmail.com>
2025-08-06 00:51:04 -0400

    changed logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <h2 class="text_logo">Le<span>Stat</span><span><img class="logo_icon"src="./images/logo_icon.png" alt="icon"/></span></h2>
+            <h2 class="text_logo">Le<span>Stat</span><span><img class="logo_icon"src="./images/logo_icon(2).svg" alt="icon"/></span></h2>
             <ul class="nav-links">
                 <li><a href="#about">About</a></li>
                 <li><a href="mailto:rnguyen2102@gmail.com">Contact</a></li>
